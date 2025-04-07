02246615e49952c6d9f0b84efae9be2d5cf36017
Ned <iamcalledned@gmail.com>
2025-04-07 17:43:21 -0400

    Update fetch_fred.py

--- data/fetch_fred.py
+++ data/fetch_fred.py
@@ -1,18 +1,8 @@
 from fredapi import Fred
 import pandas as pd
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # 👈 Load variables from .env
 
 fred = Fred(api_key=os.getenv("FRED_API_KEY"))
-
-def get_vix():
-    data = fred.get_series("VIXCLS").dropna()
-    return data
-
-def get_yield_spread():
-    ten = fred.get_series("GS10")
-    two = fred.get_series("GS2")
-    spread = ten - two
-    return spread.dropna()
-
-def get_dxy():
-    return fred.get_series("DTWEXBGS").dropna()
